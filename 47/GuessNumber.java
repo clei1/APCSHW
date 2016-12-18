e66c8a896fee5700ba460bc413ab9e00128d5afe
clei1 <clei1@stuy.edu>
2016-12-18 16:40:03 -0500

    finished GuessNumber

--- 47/GuessNumber.java
+++ 47/GuessNumber.java
@@ -14,11 +14,11 @@ public class GuessNumber{
     private int upper;
     private int userIn;
 
-    public GuessNumber{
+    public GuessNumber(){
 	lower = 1;
 	upper = 100;
 	nguess = 0;
-	number = (int) (Math.random() * 101); //Math.random() returns [0,1)
+	number = (int) (Math.random() * 100) + 1; //Math.random() returns [0,1)
 	userIn = -1;
     }
     
@@ -26,9 +26,25 @@ public class GuessNumber{
 	while(userIn != number){
 	    System.out.println("Guess a number from " + lower + "-" + upper + ":");
 	    userIn = Keyboard.readInt();
-	    nguess += 1;
+	    if( userIn >= lower && userIn <= upper){
+		nguess += 1;
+		if(userIn > number){
+		    upper = userIn-1;
+		}
+		else{
+		    lower = userIn+1;
+		}
+	    }
+	    else{
+		System.out.println("Number is not within range.");
+	    }				 
 	}
-	System.out.println("Correct! It took " + nguess + "guesses.");	
+	System.out.println("Correct! It took " + nguess + " guesses.");	
+    }
+
+    public static void main(String[] args){
+	GuessNumber test = new GuessNumber();
+	test.game();
     }
     
 }
